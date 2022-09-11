415112b4d6600ff67b8869cd0f007e30807e1065
0xNuit <digimonegg@gmail.com>
2022-09-11 20:51:35 +0300

    make it be

--- src/Posts/actions/upload-image.hook.js
+++ src/Posts/actions/upload-image.hook.js
@@ -6,18 +6,21 @@ const AdminBro = require('admin-bro');
 /** @type {AdminBro.After<AdminBro.ActionResponse>} */
 const after = async (response, request, context) => {
   const { record, uploadImage } = context;
+  try {
+    if (record.isValid() && uploadImage) {
+      // const filePath = path.join('uploads', uploadImage.name);
+      // await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
 
-  if (record.isValid() && uploadImage) {
-    const filePath = path.join('uploads', uploadImage.name); 
-    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+      // await fs.promises.rename(uploadImage.path, filePath);
 
-    await fs.promises.rename(uploadImage.path, filePath);
- 
 
-    await record.update({ profilePhotoLocation: `/${filePath}` });
-    // await record.update({ formData: uploadImage.name });
+      // await record.update({ profilePhotoLocation: `/${filePath}` });
+      // await record.update({ formData: uploadImage.name });
+    }
+    return response;
+  } catch (e) {
+    console.log(e);
   }
-  return response;
 };
 
 /** @type {AdminBro.Before} */
